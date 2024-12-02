2156d5015c41aae3c6a65275c77a54d24649ba7d
Shirosaka <shiro@shirosaka.dev>
2024-12-02 21:17:33 +0100

    remove anyhow

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,5 @@
-use anyhow::Result;
-
-fn parse(path: &str) -> Result<(Vec<i32>, Vec<i32>)> {
-    let input = std::fs::read_to_string(path)?;
+fn parse(path: &str) -> (Vec<i32>, Vec<i32>) {
+    let input = std::fs::read_to_string(path).unwrap();
     let pairs: Vec<(i32, i32)> = input
         .lines()
         .map(|line| {
@@ -16,29 +14,25 @@ fn parse(path: &str) -> Result<(Vec<i32>, Vec<i32>)> {
 
     left.sort();
     right.sort();
-    Ok((left, right))
+    (left, right)
 }
 
-fn part1(left: Vec<i32>, right: Vec<i32>) -> anyhow::Result<i32> {
-    let dist = left.iter().fold(0, |acc, x| {
+fn part1(left: Vec<i32>, right: Vec<i32>) -> i32 {
+    left.iter().fold(0, |acc, x| {
         acc + (x - right[left.iter().position(|e| e == x).unwrap()]).abs()
-    });
-    Ok(dist)
+    })
 }
 
-fn part2(left: Vec<i32>, right: Vec<i32>) -> anyhow::Result<i32> {
-    let sim = left.iter().fold(0, |acc, x| {
+fn part2(left: Vec<i32>, right: Vec<i32>) -> i32 {
+    left.iter().fold(0, |acc, x| {
         acc + (x * (right.iter().filter(|&r| r == x).count() as i32))
-    });
-    Ok(sim)
+    })
 }
 
-fn main() -> Result<()> {
-    let (left, right) = parse("day1.txt")?;
-    let distance = part1(left.clone(), right.clone())?;
-    let similarity = part2(left.clone(), right.clone())?;
+fn main() {
+    let (left, right) = parse("day1.txt");
+    let distance = part1(left.clone(), right.clone());
+    let similarity = part2(left.clone(), right.clone());
     println!("Distance:Similarity");
     println!("{distance}:{similarity}");
-
-    Ok(())
 }
